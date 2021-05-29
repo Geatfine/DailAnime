d5a97983d0217a49f40e1a47a4f7185c17cff4ac
Geatfine <david.houssian@gmail.com>
2021-05-29 22:13:56 +0200

    2commit after debugging link Android studio to GitHub

--- app/src/main/java/com/david/dailanime/presentation/list/Anime.kt
+++ app/src/main/java/com/david/dailanime/presentation/list/Anime.kt
@@ -3,6 +3,5 @@ package com.david.dailanime.presentation.list
 class Anime(
         var mal_id : Int,
         var title: String,
-        var image_url : String,
-        var test : String
+        var image_url : String
 )
